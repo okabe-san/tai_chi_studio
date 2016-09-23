ce33c27d41eafdf649479283bcd6f060db789ca5
Alias M6 <aliasmontoya719@gmail.com>
2016-09-23 14:12:15 -0600

    Update to table names * all table names are singular now * fixes to seed and migrations files that caused errors

--- src/server/db/seeds/04_classes_users.js
+++ src/server/db/seeds/04_classes_users.js
@@ -1,15 +1,15 @@
 exports.seed = function(knex, Promise) {
   return Promise.all([
     knex('class_user').insert({
-      user_id: knex('user').where('id', 1).select('id'),
+      user_id: knex('user').where('id', 91).select('id'),
       class_id: knex('class').where('id', 1).select('id')
     }),
     knex('class_user').insert({
-      user_id: knex('user').where('id', 2).select('id'),
+      user_id: knex('user').where('id', 92).select('id'),
       class_id: knex('class').where('id', 1).select('id')
     }),
     knex('class_user').insert({
-      user_id: knex('user').where('id', 3).select('id'),
+      user_id: knex('user').where('id', 93).select('id'),
       class_id: knex('class').where('id', 1).select('id')
     })
   ]);
