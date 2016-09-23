a80b4d9d61ba60208b249724c621c5294a304f04
Akiko Okabe <aki0u0@gmail.com>
2016-09-23 10:19:07 -0600

    fixed marge merge conflict

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
-<<<<<<< HEAD
-=======
-const indexController = require('../controllers/index');
->>>>>>> 76e85bbfbe0b5dec286f1d4ba3ffb3e85fafc28c
 
 router.get('/', function (req, res, next) {
   res.render('index');
