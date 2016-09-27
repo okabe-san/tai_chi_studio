19f35600e11e4a5049a593e8ce87663e18a2482d
Akiko Okabe <aki0u0@gmail.com>
2016-09-27 14:00:44 -0600

    edit migration and seed for instructors, add basic css style

--- src/server/db/migrations/20160922175252_instructors.js
+++ src/server/db/migrations/20160922175252_instructors.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('instructors', function (table) {
     table.increments();
     table.string('names').notNullable();
-    table.string('biography').notNullable();
+    table.text('biography').notNullable();
     table.string('photo_url').notNullable();
   });
 };
