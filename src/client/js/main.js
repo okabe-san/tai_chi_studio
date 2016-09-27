9d399050eb5f11653d446068a74293592989fb0d
Akiko Okabe <aki0u0@gmail.com>
2016-09-27 15:41:56 -0600

    add google map but not working yet

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -41,3 +41,16 @@ $(document).on('click', '.edit-instructor', function() {
   const id = parseInt(tempId.value);
   window.location.href = '/instructors/edit/' + id + '';
 });
+
+/* globals google */
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
