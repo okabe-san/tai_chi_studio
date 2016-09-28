a3cd43758f77f719e30b9eb0bd5c2ca57ccf2868
Gina <gdebell@gmail.com>
2016-09-28 10:03:52 -0600

    added more stuff

--- src/server/db/knex.js
+++ src/server/db/knex.js
@@ -1,3 +1,3 @@
 const environment = process.env.NODE_ENV;
-const config = process.env.DATABASE_URL || 'postgres://localhost/library' require('../../../knexfile.js')[environment];
+const config =  require('../../../knexfile.js')[environment];
 module.exports = require('knex')(config);
