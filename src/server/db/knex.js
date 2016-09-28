f5c97ed3f8ae2ec809bde856fa6e59c4fb420fed
Phil Benz <philbenz@gmail.com>
2016-09-28 10:14:40 -0600

    push for heroku

--- src/server/db/knex.js
+++ src/server/db/knex.js
@@ -1,5 +1,5 @@
 const environment = process.env.NODE_ENV;
 const config = require('../../../knexfile.js')[environment];
 const knex = require('knex')(config);
-knex.migrate.latest();
+// knex.migrate.latest();
 module.exports = knex;
