b197a8d4846ea44feac51f252c7a53e7224e5e29
Phil Benz <philbenz@gmail.com>
2016-09-28 09:59:02 -0600

    updated database values

--- knexfile.js
+++ knexfile.js
@@ -10,5 +10,16 @@ module.exports = {
     seeds: {
       directory: __dirname + '/src/server/db/seeds'
     }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
   }
-};
+}
