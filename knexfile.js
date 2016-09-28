c5d41efe7449456639a3c29a814176752046b345
Gina <gdebell@gmail.com>
2016-09-28 10:51:36 -0600

    added more stuff

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,19 @@
+const databaseName = 'studio';
+
 module.exports = {
   development: {
     client: 'postgresql',
-    connection: {
-      database: 'studio'
+    connection: `postgres://localhost:5432/${databaseName}`,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
     },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
+  },
+  test: {
+    client: 'postgresql',
+    connection: `postgres://localhost:5432/${databaseName}_test`,
     migrations: {
       directory: __dirname + '/src/server/db/migrations'
     },
@@ -14,10 +24,12 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL,
+    port: 5432,
     migrations: {
       directory: __dirname + '/src/server/db/migrations'
     },
     seeds: {
       directory: __dirname + '/src/server/db/seeds'
     }
+  }
 };
