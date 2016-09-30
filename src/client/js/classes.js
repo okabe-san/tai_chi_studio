260c01331ab5f180e38c191f276e7327cdf326d9
Phil Benz <philbenz@gmail.com>
2016-09-30 11:52:05 -0600

    fixed spelling errors

--- src/client/js/classes.js
+++ src/client/js/classes.js
@@ -13,39 +13,29 @@ $(document).on('ready', () => {
         }
       }
     }
-
     appendSchedule($('.0100pm'), $('#0100pm'));
-
   });
-// $(window).scroll(function() {
-//     $('.dayScroll')
-//     .css('opacity', 1 - $(window)
-//     .scrollTop() / 250);
-//   });
 
-      $($id).appendTo($($className))
-    }
-    function secretary(meridian) {
-      for (let i = 1; i < 12; i++) {
-        if (i >= 10) {
-          appendSchedule($('.'+ i + '00' + meridian), $('#' + i + '00' + meridian))
-        }
-        else {
-          appendSchedule($('.0'+ i + '00' + meridian), $('#0' + i + '00' + meridian))
-        }
+  function secretary(meridian) {
+    for (let i = 1; i < 12; i++) {
+      if (i >= 10) {
+        appendSchedule($('.' + i + '00' + meridian), $('#' + i + '00' + meridian));
+      }
+      else {
+        appendSchedule($('.0' + i + '00' + meridian), $('#0' + i + '00' + meridian));
       }
     }
-    function filterDay(dayAbbrev, day) {
-      $('.' + dayAbbrev).on('click', function() {
-        $('.' + day).fadeIn('slow').css('display', 'block');
-      })
-    }
-    filterDay('mon', 'Monday');
-    filterDay('tues', 'Tuesday');
-    filterDay('wed', 'Wednesday');
-    filterDay('thurs', 'Thursday');
-    filterDay('fri', 'Friday');
-    secretary('am');
-    secretary('pm');
-
+  }
+  function filterDay(dayAbbrev, day) {
+    $('.' + dayAbbrev).on('click', function() {
+      $('.' + day).fadeIn('slow').css('display', 'block');
+    });
+  }
+  filterDay('mon', 'Monday');
+  filterDay('tues', 'Tuesday');
+  filterDay('wed', 'Wednesday');
+  filterDay('thurs', 'Thursday');
+  filterDay('fri', 'Friday');
+  secretary('am');
+  secretary('pm');
 });
