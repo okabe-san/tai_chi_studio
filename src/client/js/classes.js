cac77ed0cac64ed0b65796cba29c5e55bb5c9369
Alias M6 <aliasmontoya719@gmail.com>
2016-09-30 10:22:29 -0600

    Major changes to calender functionality * Creation of on click event handlers for days of week so that classes on that day appear * Creation of on hover event for days of week * Each class block now features an inactive add button so that a user can add the class to their schedule

--- src/client/js/classes.js
+++ src/client/js/classes.js
@@ -1,25 +1,35 @@
 $(document).on('ready', () => {
     console.log('connected to classes.js');
     function appendSchedule($id, $className) {
-      return $($id).appendTo($($className))
+      $($id).appendTo($($className))
     }
-    function secretary() {
-      for (let i = 6; i < 12; i++) {
-        if(i > 9) {
-          appendSchedule($('.'+ i + '00am'), $('#' + i + '00am'))
+    function secretary(meridian) {
+      for (let i = 1; i < 12; i++) {
+        if (i >= 10) {
+          appendSchedule($('.'+ i + '00' + meridian), $('#' + i + '00' + meridian))
         }
         else {
-          appendSchedule($('.0'+ i + '00am'), $('#0' + i + '00am'))
+          appendSchedule($('.0'+ i + '00' + meridian), $('#0' + i + '00' + meridian))
         }
       }
     }
+    function filterDay(dayAbbrev, day) {
+      $('.' + dayAbbrev).on('click', function() {
+        $('.' + day).fadeIn('slow').css('display', 'block');
+      })
+      .off()
+    }
+    filterDay('mon', 'Monday');
+    filterDay('tues', 'Tuesday');
+    filterDay('wed', 'Wednesday');
+    filterDay('thurs', 'Thursday');
+    filterDay('fri', 'Friday');
+    // $('.mon').on('click', function() {
+    //   $('.Monday').css('display', 'block');
+    // })
 
+    secretary('am');
+    secretary('pm');
 
-    appendSchedule($('.0100pm'), $('#0100pm'))
 
 });
-// $(window).scroll(function() {
-//     $('.dayScroll')
-//     .css('opacity', 1 - $(window)
-//     .scrollTop() / 250);
-//   });
